3bd04cc2a1916b4935b795dd582ebda4b40fee01
Carlos Soria <csoria@cultome.io>
2018-10-09 17:40:36 -0500

    Change models relationship

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,5 @@
 class Group < ApplicationRecord
     belongs_to :group_type
-    has_and_belongs_to_many :objectives
-
-def to_s
-   name
-end
+    has_many :groups_objectives
+    has_and_belongs_to_many :objectives, through: :groups_objectives
 end
--- app/models/objective.rb
+++ app/models/objective.rb
@@ -2,12 +2,13 @@ class Objective < ApplicationRecord
   belongs_to :tiempo
   belongs_to :user
   has_many :key_results, dependent: :destroy
-  has_and_belongs_to_many :groups
+  has_many :groups_objectives
+  has_and_belongs_to_many :groups, through: :groups_objectives
 
   accepts_nested_attributes_for :key_results
   accepts_nested_attributes_for :groups
 
-  validates_presence_of :description, :period_value, :user 
+  validates_presence_of :description, :period_value, :user
 end
 
 
